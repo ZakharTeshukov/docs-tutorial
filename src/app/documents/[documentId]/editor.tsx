a34ef586e0cf12093f7e5b45fe3baa4b47b4207b
Zakhar Teshukov <96533826+ZakharTeshukov@users.noreply.github.com>
2024-12-21 23:57:25 +0700

    Commit 2

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import StarterKit from '@tiptap/starter-kit'
-import TaskItem from '@tiptap/extension-task-item'
-import TaskList from '@tiptap/extension-task-list'
-import Table from '@tiptap/extension-table'
-import TableCell from '@tiptap/extension-table-cell'
-import TableHeader from '@tiptap/extension-table-header'
-import TableRow from '@tiptap/extension-table-row'
-import Image from '@tiptap/extension-image'
-import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+import Table from '@tiptap/extension-table';
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+import Image from '@tiptap/extension-image';
+import { useEditor, EditorContent } from '@tiptap/react';
 
 
 export const Editor = () => {
