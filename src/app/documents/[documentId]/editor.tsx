e4909dc737bdb61cb50f2e7ce6d3b7d8b280d290
Zakhar Teshukov <96533826+ZakharTeshukov@users.noreply.github.com>
2024-12-22 02:19:40 +0700

    Image resize works

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,42 +1,46 @@
 "use client";
 
-import StarterKit from '@tiptap/starter-kit';
-import TaskItem from '@tiptap/extension-task-item';
-import TaskList from '@tiptap/extension-task-list';
-import Table from '@tiptap/extension-table';
-import TableCell from '@tiptap/extension-table-cell';
-import TableHeader from '@tiptap/extension-table-header';
-import TableRow from '@tiptap/extension-table-row';
-import Image from '@tiptap/extension-image';
-import { useEditor, EditorContent } from '@tiptap/react';
-
+import StarterKit from "@tiptap/starter-kit";
+import Image from "@tiptap/extension-image";
+import ImageResize from 'tiptap-extension-resize-image';
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import { useEditor, EditorContent } from "@tiptap/react";
 
 export const Editor = () => {
-    const editor = useEditor({
-        editorProps: {
-            attributes: {
-                style: "padding-left: 56px; padding-right: 56px;",
-                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 pl-14 cursor-text",
-            }
+  const editor = useEditor({
+    editorProps: {
+      attributes: {
+        style: "padding-left: 56px; padding-right: 56px;",
+        class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 pl-14 cursor-text",
+      },
+    },
+    extensions: [
+      StarterKit,
+      Image.configure({
+        inline: true, // Allow inline images
+        HTMLAttributes: {
+          class: "custom-image image", // Add a custom class for styling
         },
-
-        extensions: [
-            StarterKit, 
-            Image,
-            TaskItem.configure({
-                nested: true,
-            }),
-            TaskList,
-            Table.configure({
-                resizable: true,
-                allowTableNodeSelection: true,
-            }),            
-            TableCell,
-            TableHeader,
-            TableRow,
-        ],
-
-        content: `
+      }),
+      ImageResize,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+      Table.configure({
+        resizable: true,
+        allowTableNodeSelection: true,
+      }),
+      TableCell,
+      TableHeader,
+      TableRow,
+    ],
+    content: `
         <table>
           <tbody>
             <tr>
@@ -51,23 +55,19 @@ export const Editor = () => {
             </tr>
           </tbody>
         </table>
+        
+      <p>Insert an image below:</p>
+      <img src="https://via.placeholder.com/150" alt="Placeholder" class="custom-image" />
       `,
-      });
+  });
 
-      Table.configure({
-        HTMLAttributes: {
-          class: '',
-        },
-      })
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visable">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
 
-    return (  
-        <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visable'>
-           <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0 '>
-            <EditorContent editor={ editor }/>
-
-           </div>
-        </div>
-     );
-}
- 
 export default Editor;
